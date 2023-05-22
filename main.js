9752b8e0862ba652e91dd5fd83bac3ce72814f3e
jsots <justinsotolongo@gmail.com>
2023-05-22 15:38:48 -0400

    Added some hovers. Now I want to add more hover effects and then add my resume pdf

--- main.js
+++ main.js
@@ -2,23 +2,33 @@
 function updateTime() {
     // Create a new Date object
     let currentTime = new Date();
-  
+
     // Get the current time components
     let hours = currentTime.getHours();
     let minutes = currentTime.getMinutes();
     let seconds = currentTime.getSeconds();
-  
+
     // Add leading zeros to the time components if they are below 10
     hours = (hours < 10 ? '0' : '') + hours;
     minutes = (minutes < 10 ? '0' : '') + minutes;
-  
+
     // Format the time as HH:MM:SS
     let formattedTime = hours + ':' + minutes;
-  
+
     // Update the content of the 'current-time' element
     document.getElementById('current-time').innerHTML = formattedTime;
-  }
-  
-  // Call updateTime function every second
-  setInterval(updateTime, 1000);
-  
+}
+
+// Call updateTime function every second
+setInterval(updateTime, 1000);  
+
+let icons = document.querySelectorAll(".round")
+
+icons.forEach((icon) => {
+    icon.addEventListener("mouseover", () => {
+        icon.classList.add("icon-hover")
+    })
+    icon.addEventListener("mouseleave", () => {
+        icon.classList.remove("icon-hover")
+    })
+})
