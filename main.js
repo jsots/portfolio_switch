64b639fec82ea892668652bcaf29c3fdf9e6af25
jsots <justinsotolongo@gmail.com>
2023-05-24 12:04:03 -0400

    Added almost all features. Now i need to move the titles to above the games and then add settings

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-// Function to update the current time
 function updateTime() {
-    // Create a new Date object
     let currentTime = new Date();
 
     // Get the current time components
@@ -19,12 +17,12 @@ function updateTime() {
     document.getElementById('current-time').innerHTML = formattedTime;
 }
 
-// Call updateTime function every second
 setInterval(updateTime, 1000);  
 
 let icons = document.querySelectorAll(".icon");
 let audio = new Audio('assets/Enter-&-Back.mp3');
-let settings = document.querySelector("#settings")
+let settings = document.querySelector("#settings");
+let games = document.querySelectorAll(".game")
 
 settings.addEventListener("mouseover", () => {
     settings.classList.add("rotate-image")
@@ -56,31 +54,29 @@ icons.forEach((icon) => {
 function playSound() {
   audio.play();
 }
-// Get all the game wrappers
+
 const gameWrappers = document.querySelectorAll('.game-wrapper');
 
-// Iterate over each game wrapper
 gameWrappers.forEach((gameWrapper) => {
     const overlay = gameWrapper.querySelector('.game-overlay');
     const gameTitle = gameWrapper.querySelector('.game-info h3').textContent;
-    
-    // Add event listeners for mouse-over and mouse-out
+    const game = gameWrapper.querySelector('.game');
+
     gameWrapper.addEventListener('mouseover', showOverlay);
     gameWrapper.addEventListener('mouseout', hideOverlay);
 
-    // Function to show the overlay
     function showOverlay() {
         overlay.style.display = 'block';
-        gameWrapper.classList.add("icon-hover")
+        gameWrapper.classList.add("icon-hover");
+        game.style.filter = 'blur(2px)';
     }
     
-    // Function to hide the overlay
     function hideOverlay() {
         overlay.style.display = 'none';
-        gameWrapper.classList.remove("icon-hover")
+        gameWrapper.classList.remove("icon-hover");
+        game.style.filter = 'none';
     }
 
-    // Set the game information and button links
     overlay.querySelector('.game-info h3').textContent = gameTitle;
 });
 
